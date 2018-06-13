8d631bd5189b84600d07364c3fce4b26605d4e7e
Tien Nguyen <pri859@sudomail.com>
2018-06-13 19:08:00 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -23,7 +23,6 @@ def position_taken?(board, index)
   end
 
 def valid_move?(board, index)
-  input = gets.strip
   index = input_to_index(user_input)
 if (index).between?(0, 8) || position_taken?(board, index) == nil
   return true
