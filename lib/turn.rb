126144f89f1534167101dc59db6f723efda58fdc
Tien Nguyen <pri859@sudomail.com>
2018-06-13 19:13:59 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -34,7 +34,7 @@ def turn(board)
 until valid_move?(board, index) == true
   puts "Please enter 1-9:"
   index = gets.strip
-  index = input_to_index(user_input)
+  index = input_to_index(input)
 return false
 end
 move(board, index, character = "X")
